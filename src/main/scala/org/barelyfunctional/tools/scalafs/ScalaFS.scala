aaccdfb7b1a50958f5548db33619a213da90992c
Nathan Matthews <nathan.r.matthews@gmail.com>
2014-05-09 15:16:59 +0100

    Added basic interfaces

--- src/main/scala/org/barelyfunctional/tools/scalafs/ScalaFS.scala
+++ src/main/scala/org/barelyfunctional/tools/scalafs/ScalaFS.scala
@@ -1,1 +1,17 @@
 package org.barelyfunctional.tools.scalafs
+
+import java.io.InputStream
+
+trait Entry {
+  def name : String
+}
+
+trait File extends Entry {
+  def length : Long
+  def content : InputStream
+}
+
+trait Directory extends Entry {
+  def list : List[Entry]
+}
+
