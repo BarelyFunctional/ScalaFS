306f2937353608177d6351e787fd1aa2587316af
Nathan Matthews <nathan.r.matthews@gmail.com>
2014-05-09 15:32:40 +0100

    Added java.io.File implementation

--- src/main/scala/org/barelyfunctional/tools/scalafs/ScalaFS.scala
+++ src/main/scala/org/barelyfunctional/tools/scalafs/ScalaFS.scala
@@ -1,6 +1,6 @@
 package org.barelyfunctional.tools.scalafs
 
-import java.io.InputStream
+import java.io.{File => JFile, FileInputStream, InputStream}
 
 trait Entry {
   def name : String
@@ -14,4 +14,33 @@ trait File extends Entry {
 trait Directory extends Entry {
   def list : List[Entry]
 }
+
+object File {
+  def apply(source : JFile) = new File {
+    override def name: String = source.getName
+
+    override def content: InputStream = new FileInputStream(source)
+
+    override def length: Long = source.length
+  }
+}
+
+object Directory {
+  def apply(source : JFile) = new Directory {
+    override def name: String = source.getName
+
+    override def list: List[Entry] = source.listFiles.toList.map(Entry.apply(_))
+  }
+}
+
+object Entry {
+
+  def apply(file : JFile) : Entry =
+    if (file.isDirectory) Directory(file)
+    else File(file)
+
+  def apply(path : String) = Entry(new JFile(path))
+}
+
+
 
